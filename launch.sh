697826e801d7315bb24e903678881b6e4f2e9df0
Ori Komemi <orikom123@gmail.com>
2026-02-05 18:42:36 +0200

    PP dynamic lookahead and PID by curve

--- launch.sh
+++ launch.sh
@@ -6,4 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/install/setup.bash"
 
 # Launch the application
-ros2 launch autonomous_car_sim autonomous_car.launch.py
+ros2 launch autonomous_car_sim autonomous_car.launch.py 
+
